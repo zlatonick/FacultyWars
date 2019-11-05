ad1c8fa95a8661c148bffc18681c453536ac661e
zlatonick <zlatonick@gmail.com>
2019-11-05 17:20:21 +0200

    Initial commit. Adding main menu scene and Play button

---
+++ Assets/Scripts/MainMenu/MainMenuEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    public void onPlayButtonPressed()
+    {
+        Debug.Log("Play button pressed");
+        SceneManager.LoadScene("Preparing");
+    }
+}
