a751a36a5ae96bbc89ff7fc432fd426a8126d25e
leonid <lionshostka@gmail.com>
2019-11-21 17:27:40 +0200

    fixed constraints problem with scroll view, changed background, updated animations, created PreparingData class for storing data and providing this depending on the chosen faculty

---
+++ Assets/Scripts/Preparing/PreparingData.cs
@@ -0,0 +1,8 @@
+namespace Preparing
+{
+    public class PreparingData
+    {
+        public static int preparingCardsCount = 10;
+        
+    }
+}
--- Assets/Scripts/Preparing/SnapScrolling.cs
+++ Assets/Scripts/Preparing/SnapScrolling.cs
@@ -1,119 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class SnapScrolling : MonoBehaviour
+namespace Preparing
 {
-
-	[Range(1, 50)]
-	[Header("Controllers")]
-	public int cardCount;
-
-    [Range(0, 500)]
-	public int cardOffset;
-
-    [Range(0f, 20f)]
-	public float snapSpeed;
-
-    [Range(0f, 5f)]
-	public float scaleOffset;
-
-	[Header("Other objects")]
-	public GameObject cardPrefab;
-	public ScrollRect scrollRect;
-
-	private GameObject[] instCards;
-	private Vector2[] cardsPos;
-	private Vector2[] cardsScale;
-
-	private RectTransform contentRect;
-	private Vector2 contentVector;
-
-	private int selectedCardId;
-	private bool isScrolling;
-
-	private void Start()
+    public class SnapScrolling : MonoBehaviour
     {
-		contentRect = GetComponent<RectTransform>();
-		instCards = new GameObject[cardCount];
-		cardsPos = new Vector2[cardCount];
-		cardsScale = new Vector2[cardCount];
+        [Header("Other objects")] public GameObject cardPrefab;
 
-		for (int i = 0; i < cardCount; i++)
-		{
-			instCards[i] = Instantiate(cardPrefab, transform, false);
-			if (i == 0) continue;
+        [Range(0, 500)] public int cardOffset;
+        [Range(0f, 20f)] public float snapSpeed;
 
-			instCards[i].transform.localPosition = new Vector2(
-                instCards[i - 1].transform.localPosition.x + cardPrefab.GetComponent<RectTransform>().sizeDelta.x + cardOffset,
-				instCards[i].transform.localPosition.y
-			);
-			cardsPos[i] = -instCards[i].transform.localPosition;
-		}
-	}
+        public ScrollRect scrollRect;
 
-    private void FixedUpdate()
-	{
-        if (contentRect.anchoredPosition.x >= cardsPos[0].x && !isScrolling || contentRect.anchoredPosition.x <= cardsPos[cardsPos.Length - 1].x && !isScrolling)
-		{
-			scrollRect.inertia = false;
-		}
+        private GameObject[] _instCards;
+        private Vector2[] _cardsPos;
+        private Vector2[] _cardsScale;
 
-		float nearestPosition = float.MaxValue;
-        for (int i = 0; i < cardCount; i++)
-		{
-			float distance = Mathf.Abs(contentRect.anchoredPosition.x - cardsPos[i].x);
+        private RectTransform _contentRect;
+        private Vector2 _contentVector;
 
-            if (distance < nearestPosition)
-			{
-				nearestPosition = distance;
-				selectedCardId = i;
-			}
+        private int _selectedCardId;
+        private bool _isScrolling;
 
-			//float scale = Mathf.Clamp(1 / (distance / cardOffset) * scaleOffset, 0.5f, 1f);
-			//cardsScale[i].x = Mathf.SmoothStep(instCards[i].transform.localScale.x, scale, 6 * Time.fixedDeltaTime);
-			//cardsScale[i].y = Mathf.SmoothStep(instCards[i].transform.localScale.y, scale, 6 * Time.fixedDeltaTime);
-			//instCards[i].transform.localScale = cardsScale[i];
-		}
+        private void Start()
+        {
+            _contentRect = GetComponent<RectTransform>();
+            _instCards = new GameObject[PreparingData.preparingCardsCount];
+            _cardsPos = new Vector2[PreparingData.preparingCardsCount];
+            _cardsScale = new Vector2[PreparingData.preparingCardsCount];
 
-        for (int i = 0; i < cardCount; i++)
-		{
-			float distance = Mathf.Abs(contentRect.anchoredPosition.x - cardsPos[i].x);
-			float scale;
-			if (nearestPosition == distance)
-			{
-				scale = Mathf.Clamp(1f, 0.5f, 1f);
-			} else
-			{
-				scale = Mathf.Clamp(0.5f, 0.5f, 1f);
-			}
+            for (var i = 0; i < PreparingData.preparingCardsCount; i++)
+            {
+                var newText = cardPrefab.GetComponentInChildren<Text>();
+                newText.text = "Aue: " + i;
 
-			cardsScale[i].x = Mathf.SmoothStep(instCards[i].transform.localScale.x, scale + 0.15f, 10 * Time.fixedDeltaTime);
-			cardsScale[i].y = Mathf.SmoothStep(instCards[i].transform.localScale.y, scale + 0.15f, 10 * Time.fixedDeltaTime);
-			instCards[i].transform.localScale = cardsScale[i];
-		}
+                _instCards[i] = Instantiate(cardPrefab, transform, false);
+                if (i == 0) continue;
 
-		float scrollVelocity = Mathf.Abs(scrollRect.velocity.x);
-        if (scrollVelocity < 400 && !isScrolling)
-		{
-			scrollRect.inertia = false;
-		}
+                _instCards[i].transform.localPosition = new Vector2(_instCards[i - 1].transform.localPosition.x + cardPrefab.GetComponent<RectTransform>().sizeDelta.x + cardOffset, _instCards[i].transform.localPosition.y);
+                _cardsPos[i] = -_instCards[i].transform.localPosition;
+            }
+        }
 
+        private void FixedUpdate()
+        {
+            if (_contentRect.anchoredPosition.x >= _cardsPos[0].x && !_isScrolling ||
+                _contentRect.anchoredPosition.x <= _cardsPos[_cardsPos.Length - 1].x && !_isScrolling)
+            {
+                scrollRect.inertia = false;
+            }
 
-		if (isScrolling || scrollVelocity > 400) return;
-		contentVector.x = Mathf.SmoothStep(contentRect.anchoredPosition.x, cardsPos[selectedCardId].x, snapSpeed * Time.fixedDeltaTime);
-		contentRect.anchoredPosition = contentVector;		
-	}
+            var nearestPosition = float.MaxValue;
+            for (var i = 0; i < PreparingData.preparingCardsCount; i++)
+            {
+                var distance = Mathf.Abs(_contentRect.anchoredPosition.x - _cardsPos[i].x);
+                if (!(distance < nearestPosition)) continue;
+                nearestPosition = distance;
+                _selectedCardId = i;
+            }
 
-    public void Scrolling(bool scroll)
-	{
-		isScrolling = scroll;
-        if (scroll)
-		{
-			scrollRect.inertia = true;
-		}
-	}
+//            for (var i = 0; i < PreparingData.preparingCardsCount; i++)
+//            {
+//                var distance = Mathf.Abs(_contentRect.anchoredPosition.x - _cardsPos[i].x);
+//                var scale = Mathf.Clamp(nearestPosition == distance ? 0.7f : 0.5f, 0.5f, 1f);
+//
+//                _cardsScale[i].x = Mathf.SmoothStep(_instCards[i].transform.localScale.x, scale + 0.15f,
+//                    10 * Time.fixedDeltaTime);
+//                _cardsScale[i].y = Mathf.SmoothStep(_instCards[i].transform.localScale.y, scale + 0.15f,
+//                    10 * Time.fixedDeltaTime);
+//                _instCards[i].transform.localScale = _cardsScale[i];
+//            }
 
+            var scrollVelocity = Mathf.Abs(scrollRect.velocity.x);
+            if (scrollVelocity < 400 && !_isScrolling)
+            {
+                scrollRect.inertia = false;
+            }
+            
+            if (_isScrolling || scrollVelocity > 400) return;
+            _contentVector.x = Mathf.SmoothStep(_contentRect.anchoredPosition.x, _cardsPos[_selectedCardId].x, snapSpeed * Time.fixedDeltaTime);
+            _contentRect.anchoredPosition = _contentVector;
+        }
 
+        public void Scrolling(bool scroll)
+        {
+            _isScrolling = scroll;
+            if (scroll) { scrollRect.inertia = true; }
+        }
+    }
 }
