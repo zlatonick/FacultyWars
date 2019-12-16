c4ad419ffebbb1e3515b6ed7a05f64930f223c4e
leonid <lionshostka@gmail.com>
2019-12-16 20:18:54 +0200

    Fixed presenting card count for each faculty

--- Assets/Scripts/Preparing/ChooseFacultyEvent.cs
+++ Assets/Scripts/Preparing/ChooseFacultyEvent.cs
@@ -12,14 +12,17 @@ namespace Preparing
             {
                 case 0:
                     StuffPack.stuffClass = StuffClass.IASA;
+                    PreparingData.cardCount = 10;
                     break;
                 
                 case 1: 
                     StuffPack.stuffClass = StuffClass.FICT;
+                    PreparingData.cardCount = 17;
                     break;
                 
                 case 2:
                     StuffPack.stuffClass = StuffClass.FPM;
+                    PreparingData.cardCount = 19;
                     break;
             }
             
